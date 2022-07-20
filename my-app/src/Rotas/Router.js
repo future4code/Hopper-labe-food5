fce81c3e426ed6a56eff6abc0f024d6e29259d55
natalliaamorim <natallia.nascimneto1@gmail.com>
2022-07-20 20:56:49 -0300

    em contrução

--- my-app/src/Rotas/Router.js
+++ my-app/src/Rotas/Router.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "../Paginas/Home/Home"
 import Cadastro from "../Paginas/Cadastro/Cadastro";
-import Endereco from "../Paginas/Cadastro/MeuEndereco";
 import Carrinho from "../Paginas/Carrinho/Carrinho";
 import Login from "../Paginas/Login/Login"
 import MeuEndereco from "../Paginas/Cadastro/MeuEndereco";
+import Perfil from "../Paginas/Perfil/Perfil";
 
 
 
@@ -18,9 +18,9 @@ export const Router = () => {
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/cadastro" element={<Cadastro/>} />
-                    <Route path="/carrinho" element={<Endereco/>} />
-                    <Route path="/login" element={<Carrinho/>} />
-                    <Route path="/perfil" element={<Login/>} />
+                    <Route path="/carrinho" element={<Carrinho/>} />
+                    <Route path="/login" element={<Login/>} />
+                    <Route path="/perfil" element={<Perfil/>} />
                     <Route path="/endereco" element={<MeuEndereco/>} />
                   
                 </Routes>
