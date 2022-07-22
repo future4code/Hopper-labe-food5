ffa5c1fb9fdbe6a45f0d4baa5c9b608e1e20242b
nataliaheloisa <nataliaheloisa95@gmail.com>
2022-07-21 21:06:24 -0300

    Renomeei as páginas com jsx

--- my-app/src/Paginas/Login/Login.jsx
+++ my-app/src/Paginas/Login/Login.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
-// import {goToCadastro, goToHome} from ".../Rotas/Cordenador.js";
+import { BASE_URL } from "../../constants/Constants";
+
 
 
 
  function Login() {
+  // const body = {
+  //   "email":"",
+  //   "password":""
+  // }
+
+
 
   const navigate = useNavigate()
 
   return (
     <div>
-      <div><h3>Entrar</h3></div>
-      <form>
       <div>
+        <h3>Entrar</h3>
+      </div>
+      <form>
         <input
         type={"email"}
         placeholder="email@email.com"> e-mail</input>
@@ -20,7 +28,6 @@ import {useNavigate} from "react-router-dom";
         type={"password"}
         placeholder="Mínimo 6 caracteres"> Senha</input>
         <button onClick={() => navigate("/")}>Entrar</button>
-      </div>
       </form>
       <button onClick={() =>navigate("/cadastro")} className="">Não possui cadastro? Clique aqui.</button>
     </div>
