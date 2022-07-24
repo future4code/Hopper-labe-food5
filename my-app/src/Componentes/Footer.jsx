830e55d19c8983caa2ac723cb6d4b78e02685278
nataliaheloisa <nataliaheloisa95@gmail.com>
2022-07-24 20:53:01 -0300

    Ajustando conflitos

--- my-app/src/Componentes/Footer.jsx
+++ my-app/src/Componentes/Footer.jsx
@@ -1,65 +1,52 @@
 import React from "react";
-<<<<<<< HEAD
-import {AiOutlineHome} from 'react-icons/ai'
-import {BsCart3, BsPerson} from 'react-icons/bs'
-import { useNavigate } from "react-router-dom";
-// import { goToCarrinho, goToHome, goToPerfil} from "../Rotas/Cordenador";
-import "./css/Componentes.css";
-=======
 import { AiOutlineHome } from 'react-icons/ai'
 import { BsCart3, BsPerson } from 'react-icons/bs'
 import { useNavigate } from "react-router-dom";
-<<<<<<<< HEAD:my-app/src/Componentes/Footer.js
-import './css/Componentes.css';
-========
 // import { goToCarrinho, goToHome, goToPerfil} from "../Rotas/Cordenador";
-
->>>>>>>> master:my-app/src/Componentes/Footer.jsx
->>>>>>> master
+import "./css/Componentes.css";
+import { AiOutlineHome } from 'react-icons/ai'
+import { BsCart3, BsPerson } from 'react-icons/bs'
+import { useNavigate } from "react-router-dom";
+import './css/Componentes.css';
+// import { goToCarrinho, goToHome, goToPerfil} from "../Rotas/Cordenador";
 
 export default function Footer() {
 
   const navigate = useNavigate()
 
   return (
-<<<<<<< HEAD
     <div>
-    
-        <AiOutlineHome className="btn-footer" onClick={() => navigate("/")}/>
-    
+
+      <AiOutlineHome className="btn-footer" onClick={() => navigate("/")} />
+
       <button className="btn-footer" onClick={() => navigate("/carrinho")}>
-        <BsCart3/>
+        <BsCart3 />
       </button>
       <button className="btn-footer" onClick={() => navigate("/perfil")}>
-       <BsPerson/>
+        <BsPerson />
       </button>
-=======
-<<<<<<<< HEAD:my-app/src/Componentes/Footer.js
-    <div className="body">
-      <div className="Footer">
-        <button className="btn-footer" onClick={() => navigate("/")}>
-          <AiOutlineHome className="icon"/>
-        </button>
-        <button className="btn-footer" onClick={() => navigate("/carrinho")}>
-          <BsCart3 className="icon" />
-        </button>
-        <button className="btn-footer" onClick={() => navigate("/perfil")}>
-          <BsPerson className="icon " />
-        </button>
-      </div>
-========
-    <div>
-      <button className="" onClick={() => navigate("/")}>
-        <AiOutlineHome/>
-      </button>
-      <button className="" onClick={() => navigate("/carrinho")}>
-        <BsCart3/>
-      </button>
-      <button className="" onClick={() => navigate("/perfil")}>
-       <BsPerson/>
-      </button>
->>>>>>>> master:my-app/src/Componentes/Footer.jsx
->>>>>>> master
-    </div>
-  );
+      <div className="body">
+        <div className="Footer">
+          <button className="btn-footer" onClick={() => navigate("/")}>
+            <AiOutlineHome className="icon" />
+          </button>
+          <button className="btn-footer" onClick={() => navigate("/carrinho")}>
+            <BsCart3 className="icon" />
+          </button>
+          <button className="btn-footer" onClick={() => navigate("/perfil")}>
+            <BsPerson className="icon " />
+          </button>
+        </div>
+        <div>
+          <button className="" onClick={() => navigate("/")}>
+            <AiOutlineHome />
+          </button>
+          <button className="" onClick={() => navigate("/carrinho")}>
+            <BsCart3 />
+          </button>
+          <button className="" onClick={() => navigate("/perfil")}>
+            <BsPerson />
+          </button>
+        </div>
+        );
 }
